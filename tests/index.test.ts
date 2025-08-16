4c5b7c55e251bf85104fc89a174f5589055c0805
aldia_mechdream <futami16237@gmail.com>
2025-08-16 10:47:14 +0800

    feat: 增加模板和测试注入

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,27 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { dtsMerger } from '../src/index.js';
+import { join } from 'node:path';
+import { readFileSync, writeFileSync } from 'node:fs';
+
+const mockIndexDts = (mergeInto: string | string[]) => {
+  const into = Array.isArray(mergeInto) ? mergeInto : [mergeInto];
+  const p = join(process.cwd(), ...into);
+  const templatePath = join(process.cwd(), 'tests', 'index.d.ts.template');
+  const template = readFileSync(templatePath, 'utf-8');
+  writeFileSync(p, template);
+};
+
+const MERGE_INTO = ['tests', 'mock-dist', 'index.d.ts'];
 
 describe('dts-merger plugin', () => {
+  beforeEach(() => {
+    mockIndexDts(MERGE_INTO);
+  });
+
   it('should merge .d.ts files correctly', () => {
     const plugin = dtsMerger({
-      include: ['tests/mock-src'],
-      mergeInto: 'tests/mock-dist/index.d.ts',
+      include: [['tests', 'mock-src']],
+      mergeInto: MERGE_INTO,
       replace: {
         values: {
           __TYPE__: 'MockType',
