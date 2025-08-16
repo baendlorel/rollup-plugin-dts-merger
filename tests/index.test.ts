a61b6e7375d0b031df39a45e0772f98a20ca0478
aldia_mechdream <futami16237@gmail.com>
2025-08-16 10:17:11 +0800

    chore: test文件夹改为复数

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { dtsMerger } from '../src/index.js';
 
 describe('dts-merger plugin', () => {
-  it('should merge .d.ts files correctly', () => {});
+  it('should merge .d.ts files correctly', () => {
+    const plugin = dtsMerger({ include: ['test'] });
+  });
 });
-// todo 编写合适的测试文件
